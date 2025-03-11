bb31f6d35bbf17c0d1542371e6231756f1e379b8
Shri <adityaraje1432@gmail.com>
2025-03-11 14:27:30 +0530

    first1

---
+++ src/ex/java/basic/Lab01.java
@@ -0,0 +1,11 @@
+package ex.java.basic;
+
+public class Lab01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+    }
+
+
+}
---
+++ src/ex/java/basic/Lab02.java
@@ -0,0 +1,9 @@
+package ex.java.basic;
+
+public class Lab02 {
+    public static void main(String[] args) {
+        System.out.println("camment");
+
+//        camment for doc
+    }
+}
